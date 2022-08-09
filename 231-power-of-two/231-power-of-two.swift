1232833f312caed6d273f7e93497f4524fb3bcd1
PhiggsTarr <kgt2110@columbia.edu>
2022-08-09 09:05:46 -0400

    Time: 12 ms (21.51%), Space: 13.8 MB (31.98%) - LeetHub

--- 231-power-of-two/231-power-of-two.swift
+++ 231-power-of-two/231-power-of-two.swift
@@ -1,28 +1,46 @@
 class Solution {
     func isPowerOfTwo(_ n: Int) -> Bool {
-             var n = Double(String("\(n).0"))!
-              guard n != 1 else { return true}
-              guard n != 2 else { return true}
-              
-              
-              while n > 2.0{
-                  
-                  if n / 2.0 >= 2.0 {
-                      n = n / 2.0
-                  }else{
-                      break
-                  }
-                  
-                    if n == 2.0{
-                      return true
-                  }
-                 
-                  
-              }
-              
-              return false
+            guard n != 1 else { return true}             
+            guard n != 2 else { return true}
+        var two = 2
+        
+        while two < n{
+            two *= 2
+            if two == n{
+                return true
+            }
+        }
+        
+        return false
+        
         
     }
+    
+//      func isPowerOfTwo(_ n: Int) -> Bool {
+//              var n = Double(String("\(n).0"))!
+//               guard n != 1 else { return true}
+//               guard n != 2 else { return true}
+              
+              
+//               while n > 2.0{
+                  
+//                   if n / 2.0 >= 2.0 {
+//                       n = n / 2.0
+//                   }else{
+//                       break
+//                   }
+                  
+//                     if n == 2.0{
+//                       return true
+//                   }
+                 
+                  
+//               }
+              
+//               return false
+        
+//     }
+    
     
 //       func isPowerOfTwo(_ n: Int) -> Bool {
 //          var n = n
