09de5b6711009a55cd6d1cd6ab6af6bb463c4e89
PhiggsTarr <kgt2110@columbia.edu>
2022-08-08 21:48:53 -0400

    Time: 0 ms (100.00%), Space: 13.8 MB (31.98%) - LeetHub

--- 231-power-of-two/231-power-of-two.swift
+++ 231-power-of-two/231-power-of-two.swift
@@ -1,18 +1,46 @@
 class Solution {
-//     func isPowerOfTwo(_ n: Int) -> Bool {
-//          var n = Double(String("\(n).0"))!
+    func isPowerOfTwo(_ n: Int) -> Bool {
+         var n = Double(String("\(n).0"))!
+              guard n != 1 else { return true}
+              guard n != 2 else { return true}
+              
+              
+              while n > 2.0{
+                  
+                  if n / 2.0 >= 2.0 {
+                      n = n / 2.0
+                    if n == 2.0{
+                      return true
+                  }
+                  }else{
+                      break
+                  }
+                 
+                  
+              }
+              
+              return false
+        
+    }
+    
+//       func isPowerOfTwo(_ n: Int) -> Bool {
+//          var n = n
 //               guard n != 1 else { return true}
 //               guard n != 2 else { return true}
               
               
-//               while n > 2.0{
+//               while n > 2{
                   
-//                   if n / 2.0 >= 2.0 {
-//                       n = n / 2.0
+//                   if n % 2 != 0 {
+//                       return false
+//                   }
+                  
+//                   if n / 2 >= 2 {
+//                       n = n / 2
 //                   }else{
 //                       break
 //                   }
-//                   if n == 2.0{
+//                   if n == 2{
 //                       return true
 //                   }
                   
@@ -21,32 +49,5 @@ class Solution {
 //               return false
         
 //     }
-    
-      func isPowerOfTwo(_ n: Int) -> Bool {
-         var n = n
-              guard n != 1 else { return true}
-              guard n != 2 else { return true}
-              
-              
-              while n > 2{
-                  
-                  if n % 2 != 0 {
-                      return false
-                  }
-                  
-                  if n / 2 >= 2 {
-                      n = n / 2
-                  }else{
-                      break
-                  }
-                  if n == 2{
-                      return true
-                  }
-                  
-              }
-              
-              return false
-        
-    }
     
 }
