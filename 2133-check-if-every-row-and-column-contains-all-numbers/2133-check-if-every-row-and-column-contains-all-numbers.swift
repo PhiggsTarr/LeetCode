364b0d79de939b614f13971707f69a747a27a145
PhiggsTarr <kgt2110@columbia.edu>
2022-08-28 01:44:49 -0400

    Time: 1668 ms (18.52%), Space: 14.3 MB (74.07%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func checkValid(_ matrix: [[Int]]) -> Bool {
+        let matrixCount = matrix[0].count
+        let sortedNumbers = Array(1...matrixCount)
+        let flat = matrix.flatMap{$0}
+        var columnArray = [Int]()
+        
+        for numbers in matrix{
+            if Set(numbers).sorted() != sortedNumbers.sorted(){
+                return false
+            }
+        }
+        
+        for numbers in 0..<matrixCount{
+            
+            for index in stride(from:numbers, to:flat.count, by: matrixCount){
+                columnArray.append(flat[index])
+            }
+           
+            if columnArray.sorted() != sortedNumbers.sorted(){
+                return false
+            }
+            columnArray.removeAll()
+            
+        }
+        return true
+        
+    }
+}
